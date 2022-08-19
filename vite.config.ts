fe30a459c160ff3e99da55aa2ade38feba44988d
Andrew Nowak <andrew@nowak.me.uk>
2022-08-19 12:05:38 +0100

    fix base path for pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+  const common = { plugins: [react()] };
+
+  if (mode === "pages") {
+    return { ...common, base: "/infinigrid/" };
+  }
+
+  return common;
+});
